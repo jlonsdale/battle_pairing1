3212ee33c6e02c5f3c5a6e6b757dd3edd471cc45
Mezela <p_mezue@hotmail.com>
2019-10-01 15:06:09 +0100

    add a test-helper method

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Entering names' do
   scenario 'start fight by entering names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Pamela'
-    fill_in :player_2_name, with: 'Jess'
-    click_button('submit')
+    sign_in_and_play
     expect(page).to have_content "Pamela vs. Jess"
   end
 end
--- spec/features/viewing_hitpoints_spec.rb
+++ spec/features/viewing_hitpoints_spec.rb
@@ -1,10 +1,6 @@
 feature 'View hit points' do
   scenario 'be able to view player 2s hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'Pamela'
-    fill_in :player_2_name, with: 'Jess'
-    click_button('submit')
-    # visit('/play')
+    sign_in_and_play
     expect(page).to have_content("Jess: 100HP")
   end
 end
