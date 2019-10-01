df8fe262e5ecd81e4d8611715c7355e3262fb825
Jessie <lonsdale.jess@gmail.com>
2019-10-01 14:36:33 +0100

    enter names now working

--- spec/features/entering_names_spec.rb
+++ spec/features/entering_names_spec.rb
@@ -1,9 +1,9 @@
 feature 'Entering names' do
   scenario 'start fight by entering names' do
     visit('/')
-    fill_in('Player 1', with: 'Pamela')
-    fill_in('Player 2', with: 'Jess')
+    fill_in :player_1_name, with: 'Pamela'
+    fill_in :player_2_name, with: 'Jess'
     click_button('submit')
-    expect(page).to have_content "Player 1: Pamela \nPlayer 2: Jess"
+    expect(page).to have_content "Pamela vs. Jess"
   end
 end
