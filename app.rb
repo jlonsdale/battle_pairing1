030581f72bb1b9fa0ed434ae8481faf77e92d512
Jessie <lonsdale.jess@gmail.com>
2019-10-01 14:45:44 +0100

    sessions working

--- app.rb
+++ app.rb
@@ -1,17 +1,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  set :sessions, true
+  enable :sessions
 
   get '/' do
     erb(:index)
-  end 
+  end
 
   post '/names' do
-    p params
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb(:play)
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
